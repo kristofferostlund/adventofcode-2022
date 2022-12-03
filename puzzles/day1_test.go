e937c229df2808b6f98417a8a1c47feef60882b7
Kristoffer Östlund <kristoffer@instabox.se>
2022-12-03 10:53:15 +0100

    refactor: Group day 1 tests

--- puzzles/day1_test.go
+++ puzzles/day1_test.go
@@ -9,7 +9,8 @@ import (
 	"github.com/kristofferostlund/adventofcode-2022/puzzles"
 )
 
-var exampleInput = `1000
+func TestDay1(t *testing.T) {
+	exampleInput := `1000
 2000
 3000
 
@@ -24,65 +25,65 @@ var exampleInput = `1000
 
 10000
 `
+	t.Run("Part1", func(t *testing.T) {
+		t.Run("example input", func(t *testing.T) {
+			want := 24000
+			got, err := puzzles.Day1{}.Part1(strings.NewReader(exampleInput))
+			if err != nil {
+				t.Fatalf("solving part 1: %v", err)
+			}
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
 
-func TestDay1_Part1(t *testing.T) {
-	t.Run("example input", func(t *testing.T) {
-		want := 24000
-		got, err := puzzles.Day1{}.Part1(strings.NewReader(exampleInput))
-		if err != nil {
-			t.Fatalf("solving part 1: %v", err)
-		}
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		t.Run("input/day1.txt", func(t *testing.T) {
+			f, err := os.Open(relative.Filepath("./input/day1.txt"))
+			if err != nil {
+				t.Fatalf("opening file: %v", err)
+			}
+			defer f.Close()
+
+			want := 72511
+			got, err := puzzles.Day1{}.Part1(f)
+			if err != nil {
+				t.Fatalf("solving part 1: %v", err)
+			}
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
 	})
 
-	t.Run("input/day1.txt", func(t *testing.T) {
-		f, err := os.Open(relative.Filepath("./input/day1.txt"))
-		if err != nil {
-			t.Fatalf("opening file: %v", err)
-		}
-		defer f.Close()
+	t.Run("Part2", func(t *testing.T) {
+		t.Run("example input", func(t *testing.T) {
+			want := 45000
+			got, err := puzzles.Day1{}.Part2(strings.NewReader(exampleInput))
+			if err != nil {
+				t.Fatalf("solving part 2: %v", err)
+			}
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
 
-		want := 72511
-		got, err := puzzles.Day1{}.Part1(f)
-		if err != nil {
-			t.Fatalf("solving part 1: %v", err)
-		}
+		t.Run("input/day1.txt", func(t *testing.T) {
+			f, err := os.Open(relative.Filepath("./input/day1.txt"))
+			if err != nil {
+				t.Fatalf("opening file: %v", err)
+			}
+			defer f.Close()
 
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
-	})
-}
+			want := 212117
+			got, err := puzzles.Day1{}.Part2(f)
+			if err != nil {
+				t.Fatalf("solving part 2: %v", err)
+			}
 
-func TestDay1_Part2(t *testing.T) {
-	t.Run("example input", func(t *testing.T) {
-		want := 45000
-		got, err := puzzles.Day1{}.Part2(strings.NewReader(exampleInput))
-		if err != nil {
-			t.Fatalf("solving part 2: %v", err)
-		}
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
-	})
-
-	t.Run("input/day1.txt", func(t *testing.T) {
-		f, err := os.Open(relative.Filepath("./input/day1.txt"))
-		if err != nil {
-			t.Fatalf("opening file: %v", err)
-		}
-		defer f.Close()
-
-		want := 212117
-		got, err := puzzles.Day1{}.Part2(f)
-		if err != nil {
-			t.Fatalf("solving part 2: %v", err)
-		}
-
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
 	})
 }
