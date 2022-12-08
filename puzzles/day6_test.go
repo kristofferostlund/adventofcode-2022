c044c0cfb11754cb8db8fe4bab0394f9fc7117e2
Kristoffer Östlund <kristoffer@instabox.se>
2022-12-08 20:20:56 +0100

    solve: Day 6

---
+++ puzzles/day6_test.go
@@ -0,0 +1,105 @@
+package puzzles_test
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/kristofferostlund/adventofcode-2022/pkg/relative"
+	"github.com/kristofferostlund/adventofcode-2022/puzzles"
+)
+
+func TestDay6(t *testing.T) {
+	t.Run("Part1", func(t *testing.T) {
+		examples := []struct {
+			sequence string
+			want     int
+		}{
+			{"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7},
+			{"bvwbjplbgvbhsrlpgdmjqwftvncz", 5},
+			{"nppdvjthqldpwncqszvftbrmjlhg", 6},
+			{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10},
+			{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11},
+		}
+
+		t.Run("example input", func(t *testing.T) {
+			for _, example := range examples {
+				exampleInput, want := example.sequence, example.want
+				t.Run(fmt.Sprintf("%s -> %d", exampleInput, want), func(t *testing.T) {
+					got, err := puzzles.Day6{}.Part1(strings.NewReader(exampleInput))
+					if err != nil {
+						t.Fatalf("solving part 1: %v", err)
+					}
+					if got != want {
+						t.Errorf("got %d, want %d", got, want)
+					}
+				})
+			}
+		})
+
+		t.Run("input/day6.txt", func(t *testing.T) {
+			f, err := os.Open(relative.Filepath("./input/day6.txt"))
+			if err != nil {
+				t.Fatalf("opening file: %v", err)
+			}
+			defer f.Close()
+
+			want := 1794
+			got, err := puzzles.Day6{}.Part1(f)
+			if err != nil {
+				t.Fatalf("solving part 1: %v", err)
+			}
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+	})
+
+	t.Run("Part2", func(t *testing.T) {
+		examples := []struct {
+			sequence string
+			want     int
+		}{
+			{"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19},
+			{"bvwbjplbgvbhsrlpgdmjqwftvncz", 23},
+			{"nppdvjthqldpwncqszvftbrmjlhg", 23},
+			{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29},
+			{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26},
+		}
+
+		t.Run("example input", func(t *testing.T) {
+			for _, example := range examples {
+				exampleInput, want := example.sequence, example.want
+				t.Run(fmt.Sprintf("%s -> %d", exampleInput, want), func(t *testing.T) {
+					got, err := puzzles.Day6{}.Part2(strings.NewReader(exampleInput))
+					if err != nil {
+						t.Fatalf("solving part 2: %v", err)
+					}
+					if got != want {
+						t.Errorf("got %d, want %d", got, want)
+					}
+				})
+			}
+		})
+
+		t.Run("input/day6.txt", func(t *testing.T) {
+			f, err := os.Open(relative.Filepath("./input/day6.txt"))
+			if err != nil {
+				t.Fatalf("opening file: %v", err)
+			}
+			defer f.Close()
+
+			want := 2851
+			got, err := puzzles.Day6{}.Part2(f)
+			if err != nil {
+				t.Fatalf("solving part 2: %v", err)
+			}
+
+			if got != want {
+				t.Errorf("got %d, want %d", got, want)
+			}
+		})
+	})
+}
